46b97a2c3c3e5172b9d921cf9db0088d5ebbb306
lenapuzzle <lenapuzzle@yahoo.com>
2022-09-08 23:10:59 -0700

    routing_practice

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+// import { useParams } from "react-router";
+import {
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+
+const Hello = (props) => {
+  return (
+    <><h1 style={{color: "red"}}>The word is: Hello</h1>
+      {/* <a href="/about">Go to About</a> */}
+      <Link to={"/hello/blue/red"}>Go to Colorful Hello</Link> | <Link to={"/home"}>Go to Home</Link> | <Link to={"/4"}>Go to Four</Link></>
+  );
+}
+
+const Home = (props) => {
+  return (
+    <><h1 style={{color: "blue"}}>Welcome!</h1>
+    <Link to={"/hello/blue/red"}>Go to Colorful Hello</Link> | <Link to={"/hello"}>Go to Hello</Link> | <Link to={"/4"}>Go to Four</Link></>
+  );
+}
+
+const Four = (props) => {
+  return (
+    <><h1 style={{color: "purple"}}>The number is: 4</h1>
+    <Link to={"/home"}>Go to Home</Link> | <Link to={"/hello/blue/red"}>Go to Colorful Hello</Link> | <Link to={"/hello"}>Go to Hello</Link></>
+  );
+}
+
+const HelloColor = (props) => {
+  return (
+    <><h1 style={{color: "blue", backgroundColor: "coral"}}>The word is: Hello</h1>
+    <Link to={"/home"}>Go to Home</Link> | <Link to={"/hello"}>Go to Hello</Link> | <Link to={"/4"}>Go to Four</Link></>
+  );
+}
+// const Location = (props) => { 
+//   const { city } = useParams(); // matches our :city in our Routes
+    
+//   return (
+//     <h1>Welcome to { city }!</h1>
+//   );
+// }
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      {/* <h1>Routing Example</h1> */}
+      <Routes>
+        <Route path="/hello" element={<Hello/>}/>
+        <Route path="/home" element={<Home/>}/>
+        <Route path="/4" element={<Four/>}/>
+        <Route path="/hello/blue/red" element={<HelloColor/>}/>
+      </Routes>
+      {/* <p>
+        <Link to="/location/Seattle">Seattle</Link>
+          | 
+        <Link to="/location/Chicago">Chicago</Link>
+          | 
+        <Link to="/location/Burbank">Burbank</Link>
+      </p>
+      <Routes>
+        <Route path="/location/:city" element={<Location />}/>
+      </Routes> */}
     </div>
   );
 }
